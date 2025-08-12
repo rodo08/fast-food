ffcfff760162988dd6b01c729164a481fd9383fa
rodo08 <rodrigo.rosalesmoya@gmail.com>
2025-08-12 12:01:53 +0200

    Add user collection to appwrite cofig

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   platform: "com.rodo08.foodordering",
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: "689b0e87002ac0c33e83",
+  userCollectionId: "689b0f600032bd3f7687",
 };
